e9ad26361999148ba7a8b84055a50c015ba038cc
USER <ok@then.com>
2018-10-15 12:59:57 +0300

    added wiki api

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,12 +9,11 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.json.*;
@@ -30,7 +29,7 @@ public class Main extends Application {
         screen1(primaryStage);
     }
 
-    public void screen1(Stage window) throws FileNotFoundException {
+    public void screen1(Stage window) throws IOException {
         BorderPane rootBorderPane = new BorderPane();
         FileInputStream logoInputStream = new FileInputStream("src/images/logo.png");
         Image logo = new Image(logoInputStream);
@@ -38,6 +37,7 @@ public class Main extends Application {
         VBox mainBox = new VBox(logoImageView);
 
         //--------Proprieties--------
+        mainBox.setStyle("-fx-background-color : #1A1A1D");
         logoImageView.setFitWidth(300);
         logoImageView.setFitHeight(300);
         mainBox.setAlignment(Pos.CENTER);
@@ -47,9 +47,9 @@ public class Main extends Application {
 
         //---------------Code----------------
 
-        defs("human");
+        //defs("human");
         twitterSearch();
-
+        wikiSearch("water");
 
 
 
@@ -61,28 +61,36 @@ public class Main extends Application {
     }
 
     public List<String> defs(String word){
-        final String app_id = "dd28d5f2";
-        final String app_key = "2ebf7763e1fb2354635b0ee9e3eed2c1";
-        List<String> definitions = null;
+        String id = "dd28d5f2";
+        String key = "2ebf7763e1fb2354635b0ee9e3eed2c1";
+        List<String> definitions = new LinkedList<>();
+        String line;
+
         try {
             URL url = new URL("https://od-api.oxforddictionaries.com:443/api/v1/entries/"+"en"+"/"+word.toLowerCase());
-            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-            urlConnection.setRequestProperty("Accept","application/json");
-            urlConnection.setRequestProperty("app_id",app_id);
-            urlConnection.setRequestProperty("app_key",app_key);
+            HttpsURLConnection urlConn = (HttpsURLConnection) url.openConnection();
+            urlConn.setRequestProperty("Accept","application/json");
+            urlConn.setRequestProperty("app_id",id);
+            urlConn.setRequestProperty("app_key",key);
 
-            // read the output from the server
-            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            BufferedReader output = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
             StringBuilder stringBuilder = new StringBuilder();
 
-            String line = null;
-            while ((line = reader.readLine()) != null) {
+            while ((line = output.readLine()) != null) {
                 stringBuilder.append(line);
             }
+            //System.out.println(stringBuilder);
+
             JSONObject obj = new JSONObject(stringBuilder.toString());
-            JSONArray definitionsJSON = obj.getJSONArray("results").getJSONObject(0).
+            JSONArray definitionsJSON = obj.
+                    getJSONArray("results").
+                    getJSONObject(0).
                     getJSONArray("lexicalEntries").
-                    getJSONObject(0).getJSONArray("entries").getJSONObject(0).getJSONArray("senses");
+                    getJSONObject(0).
+                    getJSONArray("entries").
+                    getJSONObject(0).
+                    getJSONArray("senses");
+
             for (int i = 0; i < definitionsJSON.length(); i++) {
                 System.out.println(definitionsJSON.getJSONObject(i).getJSONArray("definitions").getString(0));
                 definitions.add(definitionsJSON.getJSONObject(i).getJSONArray("definitions").getString(0));
@@ -100,9 +108,27 @@ public class Main extends Application {
 
     }
 
-    public void wikiSearch() throws MalformedURLException {
-        URL url = new URL("https://en.wikipedia.org/w/api.php");
+    public void wikiSearch(String word) throws IOException {
+        String line;
+        try {
+            URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&format=json&titles=" + word.toLowerCase());
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
 
+            BufferedReader output = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((line = output.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+
+            JSONObject mainJson = new JSONObject(stringBuilder.toString());
+            String pageId = mainJson.getJSONObject("query").getJSONObject("pages").names().getString(0);
+            String text = mainJson.getJSONObject("query").getJSONObject("pages").getJSONObject(pageId).getJSONArray("revisions").getJSONObject(0).getString("*");
+
+            System.out.println(text+"\n\n\n\n\n\n"+pageId);
+        }
+        catch (Exception e){System.out.println(e);}
     }
 
     public static void main(String[] args) {
