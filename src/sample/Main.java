ccc71a553cc388e981c886c800156c58186e54ec
USER <ok@then.com>
2018-10-16 10:11:46 +0300

    added started animation

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,21 @@
 package sample;
 
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.image.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.text.Font;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -26,6 +28,7 @@ import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 
+import javafx.util.Duration;
 import org.json.*;
 
 
@@ -48,9 +51,14 @@ public class Main extends Application {
         VBox mainBox = new VBox(logoImageView);
         TextField queryTextField = new TextField();
         Button searchButton = new Button("Search");
-        VBox searchElement = new VBox(queryTextField,searchButton);
-        HBox topBox = new HBox(searchElement);
         WebView center = new WebView();
+        ToggleGroup options = new ToggleGroup();
+        RadioButton definitionOpt = new RadioButton("Definition");
+        RadioButton wikiOpt = new RadioButton("Information");
+        RadioButton twitterOpt = new RadioButton("Opinions");
+        HBox radioOptsBox = new HBox(definitionOpt,wikiOpt,twitterOpt);
+        VBox searchElement = new VBox(queryTextField,radioOptsBox,searchButton);
+        HBox topBox = new HBox(searchElement);
         Scene scene = new Scene(root, windowWidth, windowHight);
 
 
@@ -58,8 +66,8 @@ public class Main extends Application {
 
         //--------Proprieties--------
         mainBox.setStyle("-fx-background-color : #101013");
-        topBox.setStyle("-fx-background-color : #1A1A1D");
-        topBox.setMinHeight(windowHight+100);
+        topBox.setStyle("-fx-background-color : #f0f3bd");
+        topBox.setMinHeight(windowHight+200);
         queryTextField.setFont(new Font(20));
         logoImageView.setFitWidth(300);
         logoImageView.setFitHeight(300);
@@ -67,28 +75,68 @@ public class Main extends Application {
         queryTextField.setMinHeight(50);
         searchButton.setMinWidth(160);
         searchButton.setMinHeight(50);
-        searchElement.setSpacing(40);
-        topBox.setPadding(new Insets(50));
+        definitionOpt.setScaleX(1.2);
+        definitionOpt.setScaleY(1.2);
+        wikiOpt.setScaleX(1.2);
+        wikiOpt.setScaleY(1.2);
+        twitterOpt.setScaleX(1.2);
+        twitterOpt.setScaleY(1.2);
+        searchElement.setSpacing(20);
+        radioOptsBox.setSpacing(40);
+        topBox.setPadding(new Insets(50,50,windowHight/2-200,50));
         searchElement.setAlignment(Pos.CENTER);
         mainBox.setAlignment(Pos.CENTER);
         topBox.setAlignment(Pos.CENTER);
+        radioOptsBox.setAlignment(Pos.CENTER);
         root.setContent(rootBorderPane);
         rootBorderPane.setCenter(mainBox);
         rootBorderPane.setTop(topBox);
         root.setFitToWidth(true);
         root.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         center.setMinHeight(windowHight-100);
-        rootBorderPane.setCenter(center);
+        definitionOpt.setToggleGroup(options);
+        wikiOpt.setToggleGroup(options);
+        twitterOpt.setToggleGroup(options);
+        options.selectToggle(wikiOpt);
         scene.getStylesheets().add(getClass().getResource("styling.css").toString());
 
 
         //---------------Code----------------
+        Path path = new Path();
+        path.getElements().add (new MoveTo(searchButton.getTranslateX(), searchButton.getTranslateY()));
+        path.getElements().add(new LineTo(searchButton.getTranslateX(),-windowHight));
+        PathTransition pathTransition = new PathTransition();
+
+        pathTransition.setDuration(Duration.millis(1000));
+        pathTransition.setNode(searchButton);
+        pathTransition.setPath(path);
+        pathTransition.setCycleCount(4);
+        pathTransition.setAutoReverse(true);
+
+        pathTransition.play();
+
+
         searchButton.setOnAction(value -> {
-            if (!queryTextField.getText().equals("")) {
-                String title = queryTextField.getText().toLowerCase().replace(" ","_");
-                center.getEngine().load("https://en.wikipedia.org/wiki/" + title);
-                root.setVvalue(1D);
+            //String chosenOpt = options.getSelectedToggle().getProperties().toString();
+            RadioButton chosenOpt = (RadioButton) options.getSelectedToggle();
+            switch (chosenOpt.getText()){
+                case "Definition":
+                    System.out.println("Definition");
+                    break;
+                case "Information":
+                    System.out.println("Information");
+                    String title = queryTextField.getText().toLowerCase().replace(" ","_");
+                    center.getEngine().load("https://en.wikipedia.org/wiki/" + title);
+                    rootBorderPane.setCenter(center);
+                    root.setVvalue(1D);
+                    break;
+                case "Opinions":
+                    System.out.println("Opinions");
+                    break;
+                default:
+                    System.out.println(chosenOpt);
             }
+
         });
 
 
